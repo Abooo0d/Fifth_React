592c14de80f2abb0330b25554d26e5a7191e8c91
Aboo00d <abdsadalden2001@gmail.com>
2023-12-29 00:00:00 +0200

    Creating The Business Section

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
-const Button = () => {
-  return <div>Button</div>;
+const Button = ({ styles }) => {
+  return (
+    <button
+      className={`bg-blue-gradient px-6 py-4 font-poppins font-medium text-[18px] text-primary outline-none ${styles}`}
+    >
+      Get Started
+    </button>
+  );
 };
 
 export default Button;
